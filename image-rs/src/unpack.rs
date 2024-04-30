6e6fbb8b93e952e031f9892c34dc2ef95c4efce7
Silenio Quarti <silenio_quarti@ca.ibm.com>
2024-04-30 15:12:22 -0400

    Preserve xattrs

--- image-rs/src/unpack.rs
+++ image-rs/src/unpack.rs
@@ -19,6 +19,7 @@ pub fn unpack<R: io::Read>(input: R, destination: &Path) -> Result<()> {
     let mut archive = Archive::new(input);
     archive.set_preserve_ownerships(true);
     archive.set_preserve_permissions(true);
+    archive.set_unpack_xattrs(true);
     info!("unpack call ssq");
 
     if destination.exists() {
